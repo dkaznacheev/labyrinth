a369e0dd9e06ef11eac97f4ccd8350ac975ea53f
dkaznacheev <dmitry.kaznacheev@gmail.com>
2017-12-09 01:21:47 +0300

    Added background and Start Activity

--- Labyrinth/app/src/main/java/ru/spbau/labyrinth/MainActivity.java
+++ Labyrinth/app/src/main/java/ru/spbau/labyrinth/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.TextView;
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         final OuterScrollView outerScrollView = (OuterScrollView) findViewById(R.id.outerScroll);
         final HorizontalScrollView horizontalScrollView = (HorizontalScrollView) findViewById(R.id.horizontalScroll);
---
+++ Labyrinth/app/src/main/java/ru/spbau/labyrinth/StartActivity.java
@@ -0,0 +1,25 @@
+package ru.spbau.labyrinth;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        Button launchButton = (Button)findViewById(R.id.startButton);
+        launchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
