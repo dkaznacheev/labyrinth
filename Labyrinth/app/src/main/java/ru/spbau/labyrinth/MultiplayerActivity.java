9bd630412b73e9b1f15b5bed7f74c9e61cced353
Igor Tukh <igor-tukh@yandex.ru>
2018-02-05 21:04:06 +0300

    Network: new match cancel implemented

--- Labyrinth/app/src/main/java/ru/spbau/labyrinth/MultiplayerActivity.java
+++ Labyrinth/app/src/main/java/ru/spbau/labyrinth/MultiplayerActivity.java
@@ -1,13 +1,13 @@
 package ru.spbau.labyrinth;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -62,6 +62,10 @@ public class MultiplayerActivity extends AppCompatActivity implements
                 }
                 new AlertDialog.Builder(this).setMessage(message)
                         .setNeutralButton(android.R.string.ok, null).show();
+            }
+        } else if (requestCode == RC_SELECT_PLAYERS) {
+            if (requestCode != Activity.RESULT_OK) {
+                return;
             }
         }
     }
